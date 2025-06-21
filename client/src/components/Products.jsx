2a0e2d7e324e1c498c68a6941c1b9496ee31c4b1
Alapati Sumanth <iamsumanth77@gmail.com>
2025-06-21 19:29:25 +0530

    Optimized images and updated asset references

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -10,22 +10,22 @@ function Products() {
   const navigate = useNavigate();
 
   const foodMenu = [
-    { id: 1, name: "Pizza", price: "₹250", category: "Main Course",Image:"/images/pizza.jpeg" },
-    { id: 2, name: "Burger", price: "₹180", category: "Main Course",Image:"/images/burger.jpeg" },
-    { id: 3, name: "Noodles", price: "₹150", category: "Main Course",Image:"/images/noodles.jpeg" },
-    { id: 4, name: "Paneer Tikka", price: "₹200", category: "Main Course",Image:"/images/paneertikka.jpeg" },
-    { id: 5, name: "Fries", price: "₹90", category: "Snacks",Image:"/images/fries.jpeg" },
-    { id: 6, name: "Samosa", price: "₹30", category: "Snacks",Image:"/images/samosa.jpeg" },
-    { id: 7, name: "Sandwich", price: "₹120", category: "Snacks",Image:"/images/sandwich.jpeg" },
-    { id: 8, name: "Pakoda", price: "₹60", category: "Snacks",Image:"/images/pakoda.jpeg" },
-    { id: 9, name: "Salad", price: "₹130", category: "Healthy",Image:"/images/salad.jpeg" },
-    { id: 10, name: "Fruit Bowl", price: "₹140", category: "Healthy",Image:"/images/fruitbowl.jpeg" },
-    { id: 11, name: "Sprouts", price: "₹100", category: "Healthy",Image:"/images/sprouts.jpeg" },
-    { id: 12, name: "Veg Soup", price: "₹110", category: "Healthy",Image:"/images/soup.jpeg" },
-    { id: 13, name: "Ice Cream", price: "₹100", category: "Dessert",Image :"/images/icecream.jpeg" },
-    { id: 14, name: "Gulab Jamun", price: "₹90", category: "Dessert" ,Image:"/images/julabjanum.jpeg"},
-    { id: 15, name: "Chocolate Brownie", price: "₹150", category: "Dessert",Image:"/images/brownie..jpeg" },
-    { id: 16, name: "Cake", price: "₹80", category: "Dessert",Image:"/images/cake.jpeg" }
+    { id: 1, name: "Pizza", price: "₹250", category: "Main Course",Image:"/images/pizza.webp" },
+    { id: 2, name: "Burger", price: "₹180", category: "Main Course",Image:"/images/burger.webp" },
+    { id: 3, name: "Noodles", price: "₹150", category: "Main Course",Image:"/images/noodles.webp" },
+    { id: 4, name: "Paneer Tikka", price: "₹200", category: "Main Course",Image:"/images/paneertikka.webp" },
+    { id: 5, name: "Fries", price: "₹90", category: "Snacks",Image:"/images/fries.webp" },
+    { id: 6, name: "Samosa", price: "₹30", category: "Snacks",Image:"/images/samosa.webp" },
+    { id: 7, name: "Sandwich", price: "₹120", category: "Snacks",Image:"/images/sandwich.webp" },
+    { id: 8, name: "Pakoda", price: "₹60", category: "Snacks",Image:"/images/pakoda.webp" },
+    { id: 9, name: "Salad", price: "₹130", category: "Healthy",Image:"/images/salad.webp" },
+    { id: 10, name: "Fruit Bowl", price: "₹140", category: "Healthy",Image:"/images/fruitbowl.webp" },
+    { id: 11, name: "Sprouts", price: "₹100", category: "Healthy",Image:"/images/sprouts.webp" },
+    { id: 12, name: "Veg Soup", price: "₹110", category: "Healthy",Image:"/images/soup.webp" },
+    { id: 13, name: "Ice Cream", price: "₹100", category: "Dessert",Image :"/images/icecream.webp" },
+    { id: 14, name: "Gulab Jamun", price: "₹90", category: "Dessert" ,Image:"/images/julabjanum.webp"},
+    { id: 15, name: "Chocolate Brownie", price: "₹150", category: "Dessert",Image:"/images/brownie..webp" },
+    { id: 16, name: "Cake", price: "₹80", category: "Dessert",Image:"/images/cake.webp" }
   ];
 
   const addToCart = (item) => {
